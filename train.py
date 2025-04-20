05f032b9900b4455a81e8cc5b2c55cbaf834b5fe
Jordan LeDoux <jordan.ledoux@gmail.com>
2025-04-20 03:14:40 -0700

    Big commit refactor.

--- train.py
+++ train.py
@@ -2,13 +2,17 @@ import hydra
 
 from console import TPConsole
 from train_harness import run_training
-from config_schema import Config, GPT2Tokenizer, TiktokenTokenizer
+from config_schema import Config, GPT2Tokenizer, TiktokenTokenizer, DatasetConfig, ModelConfig, TrainingConfig
 from hydra.core.config_store import ConfigStore
 
 cs = ConfigStore.instance()
 cs.store(group="tokenizer", name="tiktoken_schema", node=TiktokenTokenizer)
 cs.store(group="tokenizer", name="gpt2_schema", node=GPT2Tokenizer)
-
+# Add these new registrations
+cs.store(group="dataset", name="dataset_schema", node=DatasetConfig)
+cs.store(group="model", name="model_schema", node=ModelConfig)
+cs.store(group="training", name="training_schema", node=TrainingConfig)
+cs.store(name="config_schema", node=Config)
 
 @hydra.main(version_base="1.3", config_path="configs", config_name="config")
 def main(cfg: Config):
